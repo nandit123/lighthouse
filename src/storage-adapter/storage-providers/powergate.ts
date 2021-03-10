970bc351c8e6ffdc65489f6cff54d481fb05aab4
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2021-03-10 13:28:47 +0530

    get and set user token to pow instance

--- src/storage-adapter/storage-providers/powergate.ts
+++ src/storage-adapter/storage-providers/powergate.ts
@@ -7,13 +7,29 @@ import { PowergateConfig, Data } from "../interfaces";
 
 class Powergate implements Provider {
   pow: Pow;
+  token: Promise<string>;
 
   constructor(config: PowergateConfig) {
     this.pow = createPow(config.host);
+    this.token = this.getUserToken();
+    this.setUserToken();
+  }
+
+  getUserToken(): Promise<string> {
+    return new Promise(resolve => {
+      async () => {
+        // @todo logic here for relation between ethereum address and user id/token
+        const { user } = await this.pow.admin.users.create() // save this token for later use!
+        resolve(user.token);
+      }
+    });
+  }
+
+  async setUserToken() {
+    this.pow.setToken(await this.token);
   }
 
   async store(cid: string): Promise<string> {
-    // store the data in FFS using the default storage configuration
     // store the data using the default storage configuration
     const { jobId } = await this.pow.storageConfig.apply(cid);
     return jobId;
