0f0eaad3a9073fffdf884e04756f67dcf01eec59
nandit123 <nanditmehra123@gmail.com>
2021-09-14 00:51:10 +0530

    added new functions to request and publish storage status

--- contracts/LighthouseV2.sol
+++ contracts/LighthouseV2.sol
@@ -1,7 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Lighthouse  {
-    address owner = msg.sender;
+    address owner;
+
+    constructor () {
+        owner == msg.sender;    
+    }
 
     struct Content {
         string cid;
@@ -12,12 +18,14 @@ contract Lighthouse  {
     struct Status {
         uint dealId;
         bool active;
-        mapping (uint => string) miners;
+        // mapping (uint => string) miners;
     }
 
     event StorageRequest(address uploader, string cid, string config, uint fileCost);
+    event StorageStatusRequest(address requester, string cid);
 
     mapping(address => mapping(string => Content)) public requests;
+    mapping(address => mapping(string => Status)) public status; // address -> cid -> status
 
     function store(string calldata cid, string calldata config)
         external
@@ -34,10 +42,19 @@ contract Lighthouse  {
         require(msg.sender == owner);
         recipient.transfer(amount);
     }
-    
-    fallback () external payable  {}
-    
-    function publishStorageStatus(string cid, Status status) # restrict it to only the user {
-      
+
+    function requestStorageStatus(string calldata cid) 
+        external
+    {
+        emit StorageStatusRequest(msg.sender, cid);
     }
+    
+    function publishStorageStatus(address sender, string calldata cid, uint dealId, bool active) 
+        external
+    { // restrict it to only to the user
+        require(msg.sender == owner);
+        status[sender][cid] = Status(dealId, active);
+    }
+
+    fallback () external payable  {}
 }
